c5a86f733ac71dfaa9c6447c64399f0ba30ab9c1
MengLiMing <920459250@qq.com>
2018-03-05 14:37:37 +0800

    first

---
+++ src/com/study/day1/Dog.java
@@ -0,0 +1,18 @@
+package com.study.day1;
+
+public class Dog {
+    String breed;
+    int age;
+    String color;
+    void barking() {
+
+    }
+
+    void bungry() {
+
+    }
+
+    void  sleeping() {
+
+    }
+}
